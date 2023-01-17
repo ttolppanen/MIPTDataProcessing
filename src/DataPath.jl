50a1951434de6433d504abb39517062b9eda49e2
ttolppanen <taneli.tolppanen@gmail.com>
2023-01-17 14:23:36 +0200

    fixed but with data traj number

--- src/DataPath.jl
+++ src/DataPath.jl
@@ -17,17 +17,17 @@ end
 
 function get_num_of_traj(filename, msr_prob, observable; simulation_param...)
     path_to_file = joinpath(getdatapath(), filename * ".h5")
-    h5open(path_to_file, "r") do file
-        try
+    try
+        h5open(path_to_file, "r") do file
             for g_mipt in file
                 if same_param(g_mipt, simulation_param)
                     return size(g_mipt["p = $msr_prob"][string(observable)])[2] # 1 timesteps, 2 trajectories
                 end
             end
             return 0;
-        catch
-            return 0;
         end
+    catch
+        return 0;
     end
 end
 
