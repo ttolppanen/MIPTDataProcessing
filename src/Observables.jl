e4a774f83058189e1e7cbd6df4572261e4b9d9e5
ttolppanen <taneli.tolppanen@gmail.com>
2023-01-25 13:47:32 +0200

    bosonmean observable

--- src/Observables.jl
+++ src/Observables.jl
@@ -4,7 +4,8 @@
 # s : state; a state from the calculated time-evolution
 function get_observables()
     observables = Dict(
-        :entanglement_half => (; exact = (s; sp...) ->  entanglement(sp[:d], sp[:L], s, Int(floor(sp[:L]/2))), mps = (s; sp...) ->  entanglement(s, Int(floor(sp[:L]/2))))
+        :entanglement_half => (; exact = (s; sp...) ->  entanglement(sp[:d], sp[:L], s, Int(floor(sp[:L]/2))), mps = (s; sp...) ->  entanglement(s, Int(floor(sp[:L]/2)))),
+        :boson_half => (; exact = (s; sp...) ->  bosonmean(sp[:d], sp[:L], s, 1:Int(floor(sp[:L]/2))), NOT_IMPLEMENTED_YET = (s; sp...) ->  0)
     )
     return observables
 end
