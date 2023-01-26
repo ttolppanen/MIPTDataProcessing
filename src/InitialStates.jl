cccc1e8dfa1842625629a92e6f0ec322e8c820bf
ttolppanen <taneli.tolppanen@gmail.com>
2023-01-26 11:02:44 +0200

    added onezero

--- src/InitialStates.jl
+++ src/InitialStates.jl
@@ -4,7 +4,8 @@ export get_initial_states
 
 function get_initial_states()
     states = Dict(
-        :zeroone => (; exact = (; sp...) -> zeroone(sp[:d], sp[:L]), mps = (; sp...) -> zeroonemps(sp[:d], sp[:L]))
+        :zeroone => (; exact = (; sp...) -> zeroone(sp[:d], sp[:L]), mps = (; sp...) -> zeroonemps(sp[:d], sp[:L])),
+        :onezero => (; exact = (; sp...) -> onezero(sp[:d], sp[:L]), mps = (; sp...) -> onezeromps(sp[:d], sp[:L]))
     )
     return states
 end
