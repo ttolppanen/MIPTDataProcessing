e23bbefe439902fa8f71587aaa920fcc00d91df9
ttolppanen <taneli.tolppanen@gmail.com>
2023-02-13 17:35:04 +0200

    added mutual information

--- src/Observables.jl
+++ src/Observables.jl
@@ -5,6 +5,7 @@
 function get_observables()
     observables = Dict(
         :entanglement_half => (; exact = entanglement_half, mps = entanglement_half_mps),
+        :mutual_information_first_to_half => (; exact = mutual_information_first_to_half, NOT_IMPLEMENTED_YET = (s; sp...) ->  "NOT_IMPLEMENTED_YET"),
         :boson_half => (; exact = boson_half, NOT_IMPLEMENTED_YET = (s; sp...) ->  "NOT_IMPLEMENTED_YET")
     )
     return observables
@@ -15,6 +16,15 @@ function entanglement_half(; sp...)
 end
 function entanglement_half_mps(; sp...)
     return s -> entanglement(s, Int(floor(sp[:L]/2)))
+end
+
+function mutual_information_first_to_half(; sp...)
+    half = Int(floor(sp[:L]/2))
+    return s -> entanglement(sp[:d], sp[:L], s, [1]) + entanglement(sp[:d], sp[:L], s, [half + 1]) - entanglement(sp[:d], sp[:L], s, [1, half + 1])
+end
+function mutual_information_first_to_half_mps(; sp...)
+    half = Int(floor(sp[:L]/2))
+    return s -> entanglement(s, [1]) + entanglement(sp[:d], sp[:L], s, [half + 1]) - entanglement(sp[:d], sp[:L], s, [1, half + 1])
 end
 
 function boson_half(; sp...)
