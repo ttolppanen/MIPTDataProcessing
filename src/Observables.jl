4bfe82bccadc7482958cd30d9911540b3bd3701c
ttolppanen <taneli.tolppanen@gmail.com>
2023-02-16 11:43:51 +0200

    added mutual_information_half_centered

--- src/Observables.jl
+++ src/Observables.jl
@@ -6,6 +6,7 @@ function get_observables()
     observables = Dict(
         :entanglement_half => (; exact = entanglement_half, mps = entanglement_half_mps),
         :mutual_information_first_to_half => (; exact = mutual_information_first_to_half, NOT_IMPLEMENTED_YET = (s; sp...) ->  "NOT_IMPLEMENTED_YET"),
+        :mutual_information_half_centered => (; exact = mutual_information_half_centered, NOT_IMPLEMENTED_YET = (s; sp...) ->  "NOT_IMPLEMENTED_YET"),
         :boson_half => (; exact = boson_half, NOT_IMPLEMENTED_YET = (s; sp...) ->  "NOT_IMPLEMENTED_YET")
     )
     return observables
@@ -23,6 +24,24 @@ function mutual_information_first_to_half(; sp...)
     return s -> entanglement(sp[:d], sp[:L], s, [1]) + entanglement(sp[:d], sp[:L], s, [half + 1]) - entanglement(sp[:d], sp[:L], s, [1, half + 1])
 end
 function mutual_information_first_to_half_mps(; sp...)
+    half = Int(floor(sp[:L]/2))
+    return s -> entanglement(s, [1]) + entanglement(sp[:d], sp[:L], s, [half + 1]) - entanglement(sp[:d], sp[:L], s, [1, half + 1])
+end
+
+function mutual_information_half_centered(; sp...)
+    if sp[:L] % 2 == 0 # even
+        half_distance = Int(sp[:L] / 2)
+        a = half_distance - floor(Int, half_distance / 2)
+        b = a + half_distance
+        return s -> entanglement(sp[:d], sp[:L], s, [a]) + entanglement(sp[:d], sp[:L], s, [b]) - entanglement(sp[:d], sp[:L], s, [a, b])
+    else
+        half_distance = floor(Int, sp[:L] / 2)
+        a = half_distance
+        b = a + half_distance
+        return s -> entanglement(sp[:d], sp[:L], s, [a]) + entanglement(sp[:d], sp[:L], s, [b]) - entanglement(sp[:d], sp[:L], s, [a, b])
+    end
+end
+function mutual_information_half_centered_mps(; sp...)
     half = Int(floor(sp[:L]/2))
     return s -> entanglement(s, [1]) + entanglement(sp[:d], sp[:L], s, [half + 1]) - entanglement(sp[:d], sp[:L], s, [1, half + 1])
 end
